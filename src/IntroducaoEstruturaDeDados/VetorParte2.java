75191f59340de9f00b3c9f1decde1fc28116c363
Lucas Fonseca <62804943+Dotlucas@users.noreply.github.com>
2023-03-07 23:04:06 -0300

    new file:   .vscode/extensions.json 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   bin/IntroducaoEstruturaDeDados/Vetor.class 	new file:   bin/IntroducaoEstruturaDeDados/VetorParte1.class 	new file:   bin/IntroducaoEstruturaDeDados/VetorParte2.class     new file:   bin/IntroducaoEstruturaDeDados/VetorParte3.class 	new file:   bin/IntroducaoEstruturaDeDados/VetorParte4.class 	new file:   bin/IntroducaoEstruturaDeDados/VetorParte5.class 	new file:   bin/IntroducaoEstruturaDeDados/VetorParte6.class 	new file:   src/IntroducaoEstruturaDeDados/VetorParte1.java 	new file:   src/IntroducaoEstruturaDeDados/VetorParte2.java 	new file:   src/IntroducaoEstruturaDeDados/VetorParte3.java 	new file:   src/IntroducaoEstruturaDeDados/VetorParte4.java 	new file:   src/IntroducaoEstruturaDeDados/VetorParte5.java 	new file:   src/IntroducaoEstruturaDeDados/VetorParte6.java

---
+++ src/IntroducaoEstruturaDeDados/VetorParte2.java
@@ -0,0 +1,16 @@
+package IntroducaoEstruturaDeDados;
+
+public class VetorParte2 {
+    
+    public static void main(String[] args) {
+        
+
+        Vetor vetor = new Vetor(2);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+    }
+
+}
